65794e33a877695b4525297199df2393600e2f05
Roman Turner <rn.turner@live.com>
2021-03-14 09:25:24 -0700

    commit tree 0f4ec7c1bf84418968f78b05e10f25a9ec546de7

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+
+class Polygon {
+  constructor(sides) {
+    this.sides = sides;
+  }
+  get countSides() {
+    return this.sides.length;
+  }
+  get perimeter() {
+    return this.sides.reduce((acc, el) => {
+      return (acc += el);
+    }, 0);
+  }
+}
+
+class Triangle extends Polygon {
+  get isValid() {
+    let [a, b, c] = this.sides;
+    return a + b <= c || a + c <= b || b + c <= a ? false : true;
+  }
+}
+
+class Square extends Polygon {
+  get isValid() {
+    let [a, ...rest] = this.sides;
+    let test = this.sides.filter((el) => el === a);
+    return test.length === 4 ? true : false;
+  }
+  get area() {
+      let [a, b, ...rest] = this.sides;
+      return a * b;
+  }
+}
+
